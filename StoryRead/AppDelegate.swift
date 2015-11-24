3a0b4e702a55288238ec938b29013451e20a28e0
teddyzhu <teddyzhu15@gmail.com>
2015-11-24 11:55:43 +0800

    Initial Commit

---
+++ StoryRead/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StoryRead
+//
+//  Created by teddyzhu on 15/11/24.
+//  Copyright © 2015年 teddyzhu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StoryRead/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StoryRead
+//
+//  Created by teddyzhu on 15/11/24.
+//  Copyright © 2015年 teddyzhu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
